971c0b0dc13bcf93a5b5d79edbdfbef3c50c05dc
foxtony656 <zptony101@gmail.com>
2021-02-23 22:29:06 +0800

    small improves

--- PythonConnector.js
+++ PythonConnector.js
@@ -13,10 +13,10 @@ class PythonConnector {
             console.log('PythonConnector – making a new connection to the python layer');
             PythonConnector.zerorpcProcess = spawn('python3', ['-u', path.join(__dirname, 'PythonServer.py')]);
             PythonConnector.zerorpcProcess.stdout.on('data', function(data) {
-                // console.info('python:', data.toString());
+                console.info('python:', data.toString());
             });
             PythonConnector.zerorpcProcess.stderr.on('data', function(data) {
-                // console.error('python:', data.toString());
+                console.error('python:', data.toString());
             });
             PythonConnector.zerorpc = new zerorpc.Client({'timeout': TIMEOUT, 'heartbeatInterval': TIMEOUT*1000});
             PythonConnector.zerorpc.connect('tcp://' + IP + ':' + PORT);
